a84492c6fa5bc6c9a0aeac75296a456840f3a3c3
Hugues Joneaux <jhugues@jhugues.fr>
2020-05-10 23:58:21 +0200

    v1.05.10
    Reduce some Cognitive Complexity.

--- core/actions/MissionActions.php
+++ core/actions/MissionActions.php
@@ -92,8 +92,8 @@ class MissionActions extends LocalActions
   {
     // On regarde les articles créés et on vérifie les données en base, si elles existent et si elles sont cohérentes entre elles.
     while (!empty($this->WpPostMissions)) {
-      $areDataOkay = true;
-      $doUpdate = false;
+      $this->areDataOkay = true;
+      $this->doUpdate = false;
       $this->WpPost = array_shift($this->WpPostMissions);
       $href = '/wp-admin/post.php?post='.$this->WpPost->getID().'&action=edit';
       $postTitle = $this->WpPost->getPostTitle();
@@ -108,112 +108,31 @@ class MissionActions extends LocalActions
         if (empty($Missions)) {
           $Mission = new Mission();
         } else {
-          $Mission = array_shift($Missions);
-          $this->strBilan .= '<br>On peut renseigner missionId du WpPost avec : '.$Mission->getId().'.';
+          $this->Mission = array_shift($Missions);
+          $this->strBilan .= '<br>On peut renseigner missionId du WpPost avec : '.$this->Mission->getId().'.';
         }
       }
 
       // On checke le Titre
-      if ($postTitle!=$Mission->getTitle()) {
+      if ($postTitle!=$this->Mission->getTitle()) {
         $this->strBilan .= '<br>Le titre doit être mis à jour en base.';
-        $doUpdate = true;
+        $this->doUpdate = true;
       }
 
-      // On checke le code
-      $postCode = $this->WpPost->getPostMeta(self::FIELD_CODE);
-      if ($postCode=='') {
-        $this->strBilan .= '<br><strong>Code</strong> : <em>'.$Mission->getCode().'</em>.';
-        update_post_meta($this->WpPost->getID(), self::FIELD_CODE, $Mission->getCode());
-        $areDataOkay = false;
-      } elseif ($postCode!=$Mission->getCode()) {
-        $this->strBilan .= '<br><strong>Code</strong> à mettre à jour.';
-        $Mission->setCode($postCode);
-        $doUpdate = true;
-      }
+      $this->checkCode();
+      $this->checkLevel();
+      $this->checkPlayer();
+      $this->checkDuration();
+      $this->checkOrigine();
 
-      // On checke le levelId
-      $levelId = $this->WpPost->getPostMeta(self::FIELD_LEVELID);
-      $Levels = $this->LevelServices->getLevelsWithFilters(array(self::FIELD_NAME=>$levelId));
-      $Level = array_shift($Levels);
-      if ($levelId=='') {
-        $this->strBilan .= '<br><strong>Difficulté</strong> <em>'.$Mission->getLevel()->getName().'</em>.';
-        update_post_meta($this->WpPost->getID(), self::FIELD_LEVELID, $Mission->getLevel()->getName());
-        $areDataOkay = false;
-      } else {
-        $levelId = $Level->getId();
-        if ($levelId!=$Mission->getLevelId()) {
-          $this->strBilan .= '<br><strong>Difficulté</strong> à mettre à jour.';
-          $Mission->setLevelId($levelId);
-          $doUpdate = true;
-        }
-      }
-
-      // On checke le playerId
-      $playerId = $this->WpPost->getPostMeta(self::FIELD_PLAYERID);
-      $Players = $this->PlayerServices->getPlayersWithFilters(array(self::FIELD_NAME=>$playerId));
-      $Player = array_shift($Players);
-      if ($playerId=='') {
-        $this->strBilan .= '<br><strong>Nombre</strong> <em>'.$Mission->getPlayer()->getName().'</em>.';
-        update_post_meta($this->WpPost->getID(), self::FIELD_PLAYERID, $Mission->getPlayer()->getName());
-        $areDataOkay = false;
-      } else {
-        $playerId = $Player->getId();
-        if ($playerId!=$Mission->getPlayerId()) {
-          $this->strBilan .= '<br><strong>Nombre</strong> à mettre à jour. ['.$playerId.';'.$Mission->getPlayerId().']';
-          $Mission->setPlayerId($playerId);
-          $doUpdate = true;
-        }
-      }
-
-      // On checke le durationId
-      $durationId = $this->WpPost->getPostMeta(self::FIELD_DURATIONID);
-      list($min, $max) = explode('-', $durationId);
-      $Durations = $this->DurationServices->getDurationsWithFilters(array(self::FIELD_MINDURATION=>$min, self::FIELD_MAXDURATION=>$max));
-      if (!empty($Durations)) {
-        $Duration = array_shift($Durations);
-      } else {
-        $Duration = new Duration();
-      }
-      if ($durationId=='') {
-        $strDuree = $Mission->getDuration()->getMinDuration().($Mission->getDuration()->getMaxDuration()!=0 ? '-'.$Mission->getDuration()->getMaxDuration() : '');
-        $this->strBilan .= '<br><strong>Durée</strong> <em>'.$strDuree.'</em>.';
-        update_post_meta($this->WpPost->getID(), self::FIELD_DURATIONID, $strDuree);
-        $areDataOkay = false;
-      } else {
-        $durationId = $Duration->getId();
-        if ($durationId!=$Mission->getDurationId()) {
-          $this->strBilan .= '<br><strong>Durée</strong> à mettre à jour.';
-          $Mission->setDurationId($durationId);
-          $doUpdate = true;
-        }
-      }
-
-      // On checke le origineId
-      $origineId = $this->WpPost->getPostMeta(self::FIELD_ORIGINEID);
-      $Origines = $this->OrigineServices->getOriginesWithFilters(array(self::FIELD_NAME=>$origineId));
-      $Origine = array_shift($Origines);
-      if ($origineId=='') {
-        $this->strBilan .= '<br><strong>Origine</strong> <em>'.$Mission->getOrigine()->getName().'</em>.';
-        update_post_meta($this->WpPost->getID(), self::FIELD_ORIGINEID, $Mission->getOrigine()->getName());
-        $areDataOkay = false;
-      } else {
-        $origineId = $Origine->getId();
-        if ($origineId!=$Mission->getOrigineId()) {
-          $this->strBilan .= '<br><strong>Origine</strong> à mettre à jour.';
-          $Mission->setOrigineId($origineId);
-          $doUpdate = true;
-        }
-      }
-
-
-      if (!$areDataOkay) {
+      if (!$this->areDataOkay) {
         $this->strBilan .= '<br>Analyse de l article WpPost <a href="'.$href.'">'.$postTitle.'</a> terminée, des données ne sont pas renseignées.<br>';
-      } elseif ($doUpdate) {
+      } elseif ($this->doUpdate) {
         if ($missionId==-1) {
-          $Mission->setTitle($postTitle);
-          $this->MissionServices->insertMission($Mission);
+          $this->Mission->setTitle($postTitle);
+          $this->MissionServices->insertMission($this->Mission);
         } else {
-          $this->MissionServices->updateMission($Mission);
+          $this->MissionServices->updateMission($this->Mission);
         }
         $this->strBilan .= '<br>Analyse de l article WpPost <a href="'.$href.'">'.$postTitle.'</a> terminée, avec anomalie.<br>';
       }
@@ -240,6 +159,106 @@ class MissionActions extends LocalActions
     if ($this->strBilan=='') {
       $this->strBilan = 'Il semblerait que tout aille à la perfection. Aucune anomalie remontée.';
     }
+  }
+  private function checkCode()
+  {
+    // On checke le code
+    $postCode = $this->WpPost->getPostMeta(self::FIELD_CODE);
+    if ($postCode=='') {
+      $this->addStrBilan('Code', $this->Mission->getCode());
+      update_post_meta($this->WpPost->getID(), self::FIELD_CODE, $this->Mission->getCode());
+      $this->areDataOkay = false;
+    } elseif ($postCode!=$this->Mission->getCode()) {
+      $this->strBilan .= '<br><strong>Code</strong> à mettre à jour.';
+      $this->Mission->setCode($postCode);
+      $this->doUpdate = true;
+    }
+  }
+  private function checkLevel()
+  {
+
+    // On checke le levelId
+    $levelId = $this->WpPost->getPostMeta(self::FIELD_LEVELID);
+    $Levels = $this->LevelServices->getLevelsWithFilters(array(self::FIELD_NAME=>$levelId));
+    $Level = array_shift($Levels);
+    if ($levelId=='') {
+      $this->addStrBilan('Difficulté', $this->Mission->getLevel()->getName());
+      update_post_meta($this->WpPost->getID(), self::FIELD_LEVELID, $this->Mission->getLevel()->getName());
+      $this->areDataOkay = false;
+    } else {
+      $levelId = $Level->getId();
+      if ($levelId!=$this->Mission->getLevelId()) {
+        $this->strBilan .= '<br><strong>Difficulté</strong> à mettre à jour.';
+        $this->Mission->setLevelId($levelId);
+        $this->doUpdate = true;
+      }
+    }
+  }
+  private function checkPlayer()
+  {
+    // On checke le playerId
+    $playerId = $this->WpPost->getPostMeta(self::FIELD_PLAYERID);
+    $Players = $this->PlayerServices->getPlayersWithFilters(array(self::FIELD_NAME=>$playerId));
+    $Player = array_shift($Players);
+    if ($playerId=='') {
+      $this->addStrBilan('Nombre', $this->Mission->getPlayer()->getName());
+      update_post_meta($this->WpPost->getID(), self::FIELD_PLAYERID, $this->Mission->getPlayer()->getName());
+      $this->areDataOkay = false;
+    } else {
+      $playerId = $Player->getId();
+      if ($playerId!=$this->Mission->getPlayerId()) {
+        $this->strBilan .= '<br><strong>Nombre</strong> à mettre à jour. ['.$playerId.';'.$this->Mission->getPlayerId().']';
+        $this->Mission->setPlayerId($playerId);
+        $this->doUpdate = true;
+      }
+    }
+  }
+  private function checkDuration()
+  {
+    // On checke le durationId
+    $durationId = $this->WpPost->getPostMeta(self::FIELD_DURATIONID);
+    list($min, $max) = explode('-', $durationId);
+    $Durations = $this->DurationServices->getDurationsWithFilters(array(self::FIELD_MINDURATION=>$min, self::FIELD_MAXDURATION=>$max));
+    if (!empty($Durations)) {
+      $Duration = array_shift($Durations);
+    } else {
+      $Duration = new Duration();
+    }
+    if ($durationId=='') {
+      $strDuree = $this->Mission->getDuration()->getMinDuration().($this->Mission->getDuration()->getMaxDuration()!=0 ? '-'.$this->Mission->getDuration()->getMaxDuration() : '');
+      $this->addStrBilan('Durée', $strDuree);
+      update_post_meta($this->WpPost->getID(), self::FIELD_DURATIONID, $strDuree);
+      $this->areDataOkay = false;
+    } else {
+      $durationId = $Duration->getId();
+      if ($durationId!=$this->Mission->getDurationId()) {
+        $this->strBilan .= '<br><strong>Durée</strong> à mettre à jour.';
+        $this->Mission->setDurationId($durationId);
+        $this->doUpdate = true;
+      }
+    }
+  }
+  private function checkOrigine()
+  {
+    // On checke le origineId
+    $origineId = $this->WpPost->getPostMeta(self::FIELD_ORIGINEID);
+    $Origines = $this->OrigineServices->getOriginesWithFilters(array(self::FIELD_NAME=>$origineId));
+    $Origine = array_shift($Origines);
+    if ($origineId=='') {
+      $this->addStrBilan('Origine', $this->Mission->getOrigine()->getName());
+      update_post_meta($this->WpPost->getID(), self::FIELD_ORIGINEID, $this->Mission->getOrigine()->getName());
+      $this->areDataOkay = false;
+    } else {
+      $origineId = $Origine->getId();
+      if ($origineId!=$this->Mission->getOrigineId()) {
+        $this->strBilan .= '<br><strong>Origine</strong> à mettre à jour.';
+        $this->Mission->setOrigineId($origineId);
+        $this->doUpdate = true;
+      }
+    }
+  }
+  private function addStrBilan($type, $name)
+  { $this->strBilan .=  '<br><strong>'.$type.'</strong> <em>'.$name.'</em>.';
   }
   // Fin du bloc relatif à la vérification des compétences sur la Home Admin.
   //////////////////////////////////////////////////////////////////////////////////////////////////////
